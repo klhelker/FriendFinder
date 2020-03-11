e0c3a4592a612806492522364a91d713d03826cf
klhelker <57188023+klhelker@users.noreply.github.com>
2020-03-11 16:05:31 -0500

    working on api

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,11 +7,50 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const friendQuestions = [
+    {
+    question: "Traveling has always been a priority",
+    answers: ["1", "2", "3", "4", "5"]
+    }, {
+    question: "You are typically motivated by others more than yourself.",
+    answers: ["1", "2", "3", "4", "5"]
+  }, {
+    question: "You could live without books.",
+    answers: ["1", "2", "3", "4", "5"]
+  }, {
+    question: "Skydiving is a great idea!",
+    answers: ["1", "2", "3", "4", "5"]
+  }, {
+    question: "You often say to yourself that the phrase 'Fat free' is a joke.",
+    answers: ["1", "2", "3", "4", "5"]
+  }, {
+    question: "Texting and driving is something that you find yourself doing often",
+    answers: ["1", "2", "3", "4", "5"]
+  }, {
+    question: "You typically have strong opinions and share them regardless of the room",
+    answers: ["1", "2", "3", "4", "5"]
+  }, {
+    question: "Variety is the spice of life",
+    answers: ["1", "2", "3", "4", "5"]
+  }
+  {
+    question: "Variety is the spice of life",
+    answers: ["1", "2", "3", "4", "5"]
+  }
+  {
+    question: "Variety is the spice of life",
+    answers: ["1", "2", "3", "4", "5"]
+  }
+]
+var inputItem = $("<input type='dropdown' class='answer-button'>").attr("name", questions[q].id).attr("value", questions[q].answers[i])
+
+
+
 var friendsArray = [
     {
         friendName: "Kristin",
-        friendPhoto: 
-        friendResponse: "",
+        friendPhoto: "https://images.pexels.com/photos/556665/pexels-photo-556665.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        friendResponse: 
         
     },
     
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,16 @@
 const friendData = require ("../data");
 const router = require("express").Router();
 // const "db info"
-router.get("/friends", function (req, res) {
-    return res.json();
+router.get("/api/friends", function (req, res) {
+    return res.json(friendData);
 });
 
-router.post("/friends", function(req, res){
+router.post("/api/friends", function(req, res){
     var newFriend = req.body;
     console.log(newFriend);
 })
 
+router.get("app/data/friends", function(req,res){
+    
+})
 module.exports=router
